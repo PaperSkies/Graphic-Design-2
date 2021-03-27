6f4b8738797da4a365f9dd30d7af61f98f602a20
Fractal <braden@fractal-hq.com>
2021-03-27 03:48:31 -0400

    i really should sleep

--- audio/synth.js
+++ audio/synth.js
@@ -1,12 +1,10 @@
-const bigBrainWebSynth = () => {
+const bigBrainWebSynth = (() => {
     let musicContext = new AudioContext({
 		latencyHint: 'playback',
 		sampleRate: 44100,
 	});
 	console.dir(musicContext)
 	console.log(typeof(musicContext))
-}
-
-bigBrainWebSynth(); // TODO: Delete
+})()
 
 export default bigBrainWebSynth
